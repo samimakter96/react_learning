404bac68f789ef2541dfd9d21f1c3654ecff7cf8
samim akter <samimakter9641@gmail.com>
2024-07-28 21:05:10 +0530

    redux toolkit todo

---
+++ 11-redux-toolkit-todo/src/components/AddTodo.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import {useDispatch} from 'react-redux'
+import { addTodo } from '../features/todo/todoSlice'
+
+const AddTodo = () => {
+
+  const [input, setInput] = useState('')
+  const dispatch = useDispatch()
+  
+  const addTodoHandler = (e) => {
+    e.preventDefault()
+    dispatch(addTodo(input))
+    // clear input fields
+    setInput('')
+  }
+
+  return (
+    <form onSubmit={addTodoHandler}>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder='Enter a Todo...' />
+
+      <button type="submit">Add Todo</button>
+    </form>
+  )
+}
+
+export default AddTodo
